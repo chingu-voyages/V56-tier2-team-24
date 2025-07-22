0baad4ded8e753d7b1966f4c112ff8655d650736
Hyun Woo Kim <hynwkm@gmail.com>
2025-07-22 13:57:33 -0400

    feat: add header component and add tailwind theme variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
 function App() {
-    return <></>;
+    return (
+        <BrowserRouter>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </main>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import { useMemo, useState } from "react";
+import { Link } from "react-router-dom";
+import HeaderLinks from "./HeaderLinks";
+
+export default function Header() {
+    const date = useMemo(() => new Date(), []);
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
+
+        const currentDate = date.toLocaleDateString(undefined, {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+        });
+
+    return (
+        <header className="bg-background text-bg-text h-20 flex align-center px-4">
+            <nav className="w-full max-w-7xl mx-auto flex items-center justify-between">
+                <Link to="/" className="text-xl font-semibold">
+                    Lumo
+                </Link>
+                {isLoggedIn && (
+                    <ul className="flex gap-4 text-text font-bold h-full items-center">
+                        <HeaderLinks to="/" label="Home" />
+                        <HeaderLinks to="/info" label="Patient Information" />
+                        <HeaderLinks
+                            to="/update"
+                            label="Update Patient Status"
+                        />
+                        <HeaderLinks to="/status" label="Patient Status" />
+                    </ul>
+                )}
+                <p>{currentDate}</p>
+            </nav>
+        </header>
+    );
+}
