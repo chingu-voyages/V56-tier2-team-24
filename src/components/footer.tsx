b18274c7f0b3df595832a0f538fb55d754277c0a
cfkim <christine.fth.kim@gmail.com>
2025-07-21 21:18:21 -0400

    Make footer responsive for sm,md,lg screens.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,18 +3,18 @@ export default function Footer() {
     <div className="w-full bg-[#697077] text-white">
       <div
         id="footer-content"
-        className="flex flex-col h-full justify-between items-center p-15 font-nunito text-[14px]"
+        className="flex flex-col h-full justify-between items-center p-10 md:p-15 lg:p-15 font-nunito text-[14px]"
       >
         <div
           id="footer-information"
-          className="w-full flex flex-row justify-between mb-14"
+          className="w-full flex flex-col md:flex-row lg:flex-row justify-between mb-14"
         >
-          <div className="flex flex-row gap-8">
+          <div className="flex flex-row gap-10">
             <div className="flex flex-col gap-1">
               <p>Viral</p>
               <p className="font-nunito-bold">Product owner</p>
             </div>
-            <div className="grid grid-flow-row grid-cols-4 gap-8">
+            <div className="grid grid-flow-row grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
               <div className="flex flex-col gap-1">
                 <p>Mikala</p>
                 <p className="font-nunito-bold">Scrum Master</p>
@@ -50,7 +50,7 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className="flex flex-col gap-3 items-center justify-center">
+          <div className="flex flex-col gap-3 items-center justify-center mt-10">
             <a href="https://github.com/chingu-voyages/V56-tier2-team-24">
               <img
                 src="./public/static/images/github 1.png"
