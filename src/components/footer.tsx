de216c5ae2efa95549cf9222021214136fd4b855
cfkim <christine.fth.kim@gmail.com>
2025-07-21 21:04:36 -0400

    Add bold font and link github repo in logo.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,47 +12,52 @@ export default function Footer() {
           <div className="flex flex-row gap-8">
             <div className="flex flex-col gap-1">
               <p>Viral</p>
-              <p className="font-bold">Product owner</p>
+              <p className="font-nunito-bold">Product owner</p>
             </div>
             <div className="grid grid-flow-row grid-cols-4 gap-8">
               <div className="flex flex-col gap-1">
                 <p>Mikala</p>
-                <p className="font-bold">Scrum Master</p>
+                <p className="font-nunito-bold">Scrum Master</p>
               </div>
               <div className="flex flex-col gap-1">
                 <p>Vartika</p>
-                <p className="font-bold">UX/UI Designer</p>
+                <p className="font-nunito-bold">UX/UI Designer</p>
               </div>
               <div className="flex flex-col gap-1">
                 <p>Henry</p>
-                <p className="font-bold">Developer</p>
+                <p className="font-nunito-bold">Developer</p>
               </div>
               <div className="flex flex-col gap-1">
                 <p>Claire</p>
-                <p className="font-bold">Developer</p>
+                <p className="font-nunito-bold">Developer</p>
               </div>
               <div className="flex flex-col gap-1">
                 <p>Omowumi</p>
-                <p className="font-bold">Shadow SM</p>
+                <p className="font-nunito-bold">Shadow SM</p>
               </div>
               <div className="flex flex-col gap-1">
                 <p>Khushali</p>
-                <p className="font-bold">UX/UI Designer</p>
+                <p className="font-nunito-bold">UX/UI Designer</p>
               </div>
               <div className="flex flex-col gap-1">
                 <p>Christine</p>
-                <p className="font-bold">Developer</p>
+                <p className="font-nunito-bold">Developer</p>
               </div>
               <div className="flex flex-col gap-1">
                 <p>Rel</p>
-                <p className="font-bold">Developer</p>
+                <p className="font-nunito-bold">Developer</p>
               </div>
             </div>
           </div>
 
-          <div className="flex flex-col gap-2">
-            <img src="./public/static/images/github 1.png" alt="github logo" />
-            <p>Github repo</p>
+          <div className="flex flex-col gap-3 items-center justify-center">
+            <a href="https://github.com/chingu-voyages/V56-tier2-team-24">
+              <img
+                src="./public/static/images/github 1.png"
+                alt="github logo"
+              />
+            </a>
+            <p className="font-nunito-bold">Github repo</p>
           </div>
         </div>
         <hr className="border-white w-full mb-10" />
